2614b467bac8f1819c968e6d688c63fcb29ec65e
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-02-06 21:23:39 +0530

    router, usercontroller updated with register

---
+++ backend/src/controllers/userController.js
@@ -0,0 +1,27 @@
+import { asyncHandler } from "../middleswares/asyncHandler.js";
+import { User } from "../models/userSchema.js";
+import ErrorHandler from "../middleswares/error.js";
+
+
+export const register = asyncHandler(async (req, res, next) => {
+  const { name, email, phone, password, role } = req.body;
+  if (!name || !email || !phone || !password || !role) {
+    return next(new ErrorHandler("Please fill full form!"));
+  }
+  const isEmail = await User.findOne({ email });
+  if (isEmail) {
+    return next(new ErrorHandler("Email already registered!"));
+  }
+  const user = await User.create({
+    name,
+    email,
+    phone,
+    password,
+    role,
+  });
+  res.status(200).json({
+    success: true,
+    message: "user registered",
+    user,
+  })
+});

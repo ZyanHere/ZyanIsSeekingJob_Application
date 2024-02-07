3a36486b240fdc8fa6ba87467cee264d7a55fb3c
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-02-07 16:31:47 +0530

    login, logout controller, userrouter updated

--- backend/src/controllers/userController.js
+++ backend/src/controllers/userController.js
@@ -1,6 +1,6 @@
 import { asyncHandler } from "../middleswares/asyncHandler.js";
 import { User } from "../models/userSchema.js";
-import ErrorHandler from "../middleswares/error.js";
+import ErrorHandler, { errorMiddleware } from "../middleswares/error.js";
 import { sendToken } from "../utils/jwtTokens.js";
 
 
@@ -28,3 +28,41 @@ export const register = asyncHandler(async (req, res, next) => {
   )
 });
 
+export const login = asyncHandler(async (req,res,next) => {
+  const {
+    email,
+    password,
+    role,
+  } = req.body;
+
+  if(!email || !password || !role){
+    return next(new ErrorHandler("Please fill all fields.",400))
+  }
+  const user = await User.findOne({ email }).select("+password")
+  if((!user)){
+    return new ErrorHandler("Invalid Email or Password", 400);
+  }
+
+  const isPasswordMatched = await user.comparePassword(password);
+
+  if(!isPasswordMatched){
+    return new ErrorHandler("Invalid Emial or Password",400)
+  }
+
+  if(user.role !== role){
+    return new ErrorHandler("user with this role not exist",400)
+  }
+
+  sendToken(user, 200, res, "User logged in successfully")
+});
+
+export const logout = asyncHandler(async (req,res, next) => {
+  res.status(201).cookie("token", "",{
+    httpOnly: true,
+    expires: new Date(Date.now()),
+  })
+  .json({
+    success: true,
+    message: "Use logged out successfully"
+  })
+})
--- backend/src/routes/userRouter.js
+++ backend/src/routes/userRouter.js
@@ -1,7 +1,12 @@
 import express from "express"
-import { register } from "../controllers/userController.js";
+import { login, logout, register } from "../controllers/userController.js";
+import { isAuthenticated } from "../middleswares/auth.js"
 
 const router = express.Router();
+
+
 router.post("/register", register)
+router.post("/login", login)
+router.post("/logout",isAuthenticated, logout)
 
 export default router;

68feec850993643697c3c9ae9f9324321109274d
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-09-10 00:53:45 +0530

    new app backend setup with models

---
+++ backend/index.js
@@ -0,0 +1,32 @@
+import cookieParser from 'cookie-parser';
+import express from 'express';
+import cors from 'cors';
+import dotenv from 'dotenv'
+import connectDB from './utils/db.js';
+
+dotenv.config({});
+
+const app = express();
+
+app.get("home", (req, res) => {
+    return res.status(200).json({
+        message: "Welcome to the home page!",
+        success: true,
+    })
+})
+
+//middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
+const corsOptions = {
+    origin: 'http://localhost:5173',
+    credentials: true,
+}
+app.use(cors(corsOptions))
+
+connectDB
+PORT =process.env.PORT || 3000;
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port ${PORT}`);
+})

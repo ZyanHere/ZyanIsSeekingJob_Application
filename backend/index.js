ca56301fb54c5183546d662b2a526de7842c0310
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-09-11 01:11:36 +0530

    company controller updated with routes

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ import cors from 'cors';
 import dotenv from 'dotenv'
 import connectDB from './utils/db.js';
 import userRoute from "./routes/user.route.js"
+import companyRoute from "./routes/company.route.js"
 
 dotenv.config({});
 
@@ -28,7 +29,8 @@ app.use(cors(corsOptions))
 PORT =process.env.PORT || 3000;
 
 //api
-app.use("api/v1/user", userRoute)
+app.use("api/v1/user", userRoute);
+app.use("api/v1/company", companyRoute);
 
 connectDB().
 app.listen(PORT, ()=> {
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -2,7 +2,7 @@ import express from 'express';
 import { login, logout, register, updateProfile } from '../controllers/user.controller.js';
 import isAuthenticated from '../middlewares/isAuthenticated.js';
 
-const router = express.router();
+const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);

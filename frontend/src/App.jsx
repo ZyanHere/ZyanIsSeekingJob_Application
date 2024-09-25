012efee5e7082e424331b60713847317463164b3
ZyanHere <133707817+ZyanHere@users.noreply.github.com>
2024-09-25 13:13:42 +0530

    signup page updated

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,13 +1,27 @@
-import Footer from './components/ui/shared/Footer';
-import Navbar from './components/ui/shared/Navbar';
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import Footer from "./components/shared/Footer";
+import Navbar from "./components/shared/Navbar";
+import Login from "./components/auth/Login";
+import Signup from "./components/auth/Signup";
+import Home from "./components/Home";
+
+
+const appRouter = createBrowserRouter([
+  { path: "/", element: <Home /> },
+  { path: "/login", element: <Login /> },
+  { path: "/signup", element: <Signup /> },
+  // { path: '*', component: NotFound } // This will catch all unknown routes and redirect to NotFound page.
+]);
 
 function App() {
-    return (
+  return (
     <>
-      <Navbar/>
-      {/* <Footer/> */}
+      <RouterProvider router={appRouter}>
+        <Navbar />
+        <Footer />
+      </RouterProvider>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
